6047061d33015d45d6e138d210816a303d75bc47
Qnysh <QnyshN@users.noreply.github.com>
2020-06-04 19:26:08 +0300

    App service update

--- ProjectBaker.Services/AppService.cs
+++ ProjectBaker.Services/AppService.cs
@@ -6,20 +6,62 @@ using System.Threading.Tasks;
 
 namespace ProjectBaker.Services
 {
-	public class AppService : IAppService, IUserRepository
+	public class AppService 
+		: IAppService
+		, IProjectRepository
+		, IUserRepository
+		, IJobRepository
+		, ICommentRepository
+		, IReviewRepository
+		, IRoleRepository
 	{
 		private IProjectRepository _projectRepository;
 		private IUserRepository _userRepository;
+		private IJobRepository _jobRepository;
+		private ICommentRepository _commentRepository;
+		private IReviewRepository _reviewRepository;
+		private IRoleRepository _roleRepository;
 
-		public AppService(IProjectRepository projectRepository, IUserRepository userRepository)
+
+		public AppService(
+			IProjectRepository projectRepository,
+			IUserRepository userRepository,
+			IJobRepository jobRepository,
+			ICommentRepository commentRepository,
+			IReviewRepository reviewRepository,
+			IRoleRepository roleRepository)
 		{
 			_projectRepository = projectRepository;
 			_userRepository = userRepository;
+			_jobRepository = jobRepository;
+			_commentRepository = commentRepository;
+			_reviewRepository = reviewRepository;
+			_roleRepository = roleRepository;
+		}
+
+		public void AddComment(Comment comment)
+		{
+			_commentRepository.AddComment(comment);
+		}
+
+		public void AddJob(Job user)
+		{
+			_jobRepository.AddJob(user);
 		}
 
 		public void AddProject(Project project)
 		{
 			_projectRepository.AddProject(project);
+		}
+
+		public void AddReview(Review user)
+		{
+			_reviewRepository.AddReview(user);
+		}
+
+		public void AddRole(Role user)
+		{
+			_roleRepository.AddRole(user);
 		}
 
 		public void AddUser(User user)
@@ -32,9 +74,59 @@ namespace ProjectBaker.Services
 			return _userRepository.AddUserAsync(user);
 		}
 
+		public void DeleteComment(Comment comment)
+		{
+			_commentRepository.DeleteComment(comment);
+		}
+
+		public void DeleteJob(Job job)
+		{
+			_jobRepository.DeleteJob(job);
+		}
+
+		public void DeleteProject(Project project)
+		{
+			_projectRepository.DeleteProject(project);
+		}
+
+		public void DeleteReview(Review review)
+		{
+			_reviewRepository.DeleteReview(review);
+		}
+
+		public void DeleteRole(Role role)
+		{
+			_roleRepository.DeleteRole(role);
+		}
+
+		public void DeleteUser(User user)
+		{
+			_userRepository.DeleteUser(user);
+		}
+
+		public List<Comment> GetAllComments()
+		{
+			return _commentRepository.GetAllComments();
+		}
+
+		public List<Job> GetAllJobs()
+		{
+			return _jobRepository.GetAllJobs();
+		}
+
 		public List<Project> GetAllProjects()
 		{
 			return _projectRepository.GetAllProjects();
+		}
+
+		public List<Review> GetAllReviews()
+		{
+			return _reviewRepository.GetAllReviews();
+		}
+
+		public List<Role> GetAllRoles()
+		{
+			return _roleRepository.GetAllRoles();
 		}
 
 		public List<User> GetAllUsers()
@@ -42,9 +134,39 @@ namespace ProjectBaker.Services
 			return _userRepository.GetAllUsers();
 		}
 
+		public Comment GetCommentById(int id)
+		{
+			return _commentRepository.GetCommentById(id);
+		}
+
+		public Job GetJobById(int id)
+		{
+			return _jobRepository.GetJobById(id);
+		}
+
 		public Project GetProjectById(int id)
 		{
 			return _projectRepository.GetProjectById(id);
+		}
+
+		public List<Comment> GetProjectComments(int projectId)
+		{
+			return _commentRepository.GetProjectComments(projectId);
+		}
+
+		public List<Job> GetProjectJobs(int porjectId)
+		{
+			return _jobRepository.GetProjectJobs(porjectId);
+		}
+
+		public Review GetReviewById(int id)
+		{
+			return _reviewRepository.GetReviewById(id);
+		}
+
+		public Role GetRoleById(int id)
+		{
+			return _roleRepository.GetRoleById(id);
 		}
 
 		public User GetUserByEmail(string email)
@@ -55,6 +177,81 @@ namespace ProjectBaker.Services
 		public Task<User> GetUserByEmailAsync(string email)
 		{
 			return _userRepository.GetUserByEmailAsync(email);
+		}
+
+		public List<Comment> GetUserComments(string userEmail)
+		{
+			return _commentRepository.GetUserComments(userEmail);
+		}
+
+		public List<Job> GetUserJobs(string userEmail)
+		{
+			return _jobRepository.GetUserJobs(userEmail);
+		}
+
+		public List<Project> GetUserProjects(string userEmail)
+		{
+			return _projectRepository.GetUserProjects(userEmail);
+		}
+
+		public List<Review> GetUserReviews(string userEmail)
+		{
+			return _reviewRepository.GetUserReviews(userEmail);
+		}
+
+		public List<Comment> PageComments(int skip, int take)
+		{
+			return _commentRepository.PageComments(skip, take);
+		}
+
+		public List<Job> PageJobs(int skip, int take)
+		{
+			return _jobRepository.PageJobs(skip, take);
+		}
+
+		public List<Project> PageProjects(int skip, int take)
+		{
+			return _projectRepository.PageProjects(skip, take);
+		}
+
+		public List<Review> PageReviews(int skip, int take)
+		{
+			return _reviewRepository.PageReviews(skip, take);
+		}
+
+		public List<User> PageUsers(int skip, int take)
+		{
+			return _userRepository.PageUsers(skip, take);
+		}
+
+		public void UpdateComment(Comment comment)
+		{
+			_commentRepository.UpdateComment(comment);
+		}
+
+		public void UpdateJob(Job job)
+		{
+			_jobRepository.UpdateJob(job);
+		}
+
+		public void UpdateProject(Project project)
+		{
+			_projectRepository.UpdateProject(project);
+		}
+
+		public void UpdateReview(Review review)
+		{
+			_reviewRepository.UpdateReview(review);
+		}
+
+		public void UpdateRole(Role role)
+		{
+			_roleRepository.UpdateRole(role);
+		}
+
+		public void UpdateUser(User user)
+		{
+			_userRepository.UpdateUser(user);
 		}
 
 		public User ValidateUser(User user)
